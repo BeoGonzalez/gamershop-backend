4ca6d1dd6b6db1ae9a29b3a6cd7a6aa0cbc5f0e6
Gonzalez230 <benjagonzalez230@gmail.com>
2025-11-27 14:06:24 -0300

    se igualo el nombre de las rutas

--- src/main/java/com/example/gamershop_backend/controller/ProductoController.java
+++ src/main/java/com/example/gamershop_backend/controller/ProductoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/carrito")
+@RequestMapping("/carrito")
 @CrossOrigin("*")
 public class ProductoController {
     private final ProductoRepository repo;
