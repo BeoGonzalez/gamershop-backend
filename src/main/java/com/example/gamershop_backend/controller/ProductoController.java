7da832abf2262d782e029c33691c95d79910aa12
Gonzalez230 <benjagonzalez230@gmail.com>
2025-11-27 13:45:20 -0300

    se arreglo la ruta de la api para ver si funciona

--- src/main/java/com/example/gamershop_backend/controller/ProductoController.java
+++ src/main/java/com/example/gamershop_backend/controller/ProductoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/productos")
+@RequestMapping("/api/carrito")
 @CrossOrigin("*")
 public class ProductoController {
     private final ProductoRepository repo;
