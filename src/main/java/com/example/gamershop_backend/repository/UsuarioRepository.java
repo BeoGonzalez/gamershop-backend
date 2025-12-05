dcc328d2019fe062bd002a4d66807c089d04c09c
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-12-05 01:25:31 -0300

    arregle el usuariorepository

--- src/main/java/com/example/gamershop_backend/repository/UsuarioRepository.java
+++ src/main/java/com/example/gamershop_backend/repository/UsuarioRepository.java
@@ -3,9 +3,12 @@ package com.example.gamershop_backend.repository;
 import com.example.gamershop_backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional; // <--- Importamos Optional
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    // Este método es mágico en JPA, busca por el campo 'username'
-    Usuario findByUsername(String username);
+
+    // CORRECCIÓN: Cambiamos 'Usuario' por 'Optional<Usuario>'
+    // Esto permite usar .orElseThrow() en el servicio sin errores.
+    Optional<Usuario> findByUsername(String username);
 }
